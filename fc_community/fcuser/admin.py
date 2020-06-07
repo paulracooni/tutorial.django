4e8d90b8e1a3e2efcc7614be7e1b71aa9bf74d17
Paul Kim <paulracooni@gmail.com>
2020-06-07 12:48:21 +0900

    feat: Update all unitll this repo created

---
+++ fc_community/fcuser/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Fcuser
+
+# Register your models here.
+class FcuserAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'password')
+    pass 
+
+
+admin.site.register(Fcuser, FcuserAdmin)
+
+
---
+++ fc_django/fcuser/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Fcuser
+
+# Register your models here.
+class FcuserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'password', 'register_date' )
+
+admin.site.register(Fcuser, FcuserAdmin)

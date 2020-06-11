492cf70c5612493fdd2324e69dc653cb86d989f0
paulracooni <paulracooni@gmail.com>
2020-06-11 21:18:53 +0900

    fix: Some bug found in tutorial, end solved

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from tutorial.quickstart import views
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),

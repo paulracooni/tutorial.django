f4056b0151ab2aa859e9a70cafd17ff597192035
unknown <paulkim11@soongsil.ac.kr>
2020-06-13 09:05:18 +0900

    study: Simple REST API without DRF

--- drf_api/core/views.py
+++ drf_api/core/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# Create your views here.
-def test_view(request):
+# third party imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-    data = dict(name="json", age=23)
 
-    return JsonResponse(data)
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+
+        data = dict(name="json", age=23)
+
+        return Response(data)
+
+
+# # Create your views here.
+# def test_view(request):
+
+#     data = dict(name="json", age=23)
+
+#     return JsonResponse(data)

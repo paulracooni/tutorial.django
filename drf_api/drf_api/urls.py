3ab23441716a118ce2d7e4078eadfeb9bde47a04
paulracooni <paulracooni@gmail.com>
2020-06-15 12:09:22 +0900

    study: Part 4 - Views

--- drf_api/drf_api/urls.py
+++ drf_api/drf_api/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from core.views import TestView
+from rest_framework.authtoken.views import obtain_auth_token
+from core.views import PestView, PostCreateView, PostListCreateView
 
 urlpatterns = [
     path("api-auth", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
-    path("", TestView.as_view(), name="test"),
+    path("", PestView.as_view(), name="test"),
+    path("create/", PostCreateView.as_view(), name="create"),
+    path("listcreate/", PostListCreateView.as_view(), name="listcreate"),
+    path("api/token", obtain_auth_token, name="obtain-token"),
 ]
